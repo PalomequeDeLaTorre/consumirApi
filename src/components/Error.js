79fceb24addbb183b0343843f7543abf19378269
PalomequeDeLaTorre <carlospd.ti22@utsjr.edu.mx>
2024-02-01 17:05:57 -0600

    consumirApi

--- src/components/Error.js
+++ src/components/Error.js
@@ -2,6 +2,6 @@ import React from 'react';
 
 export function Error() {
     return (
-        <h1>Error 404</h1>
+        <h1>Error 404, la página que estás buscando no se pudo encontrar</h1>
     );
 }
