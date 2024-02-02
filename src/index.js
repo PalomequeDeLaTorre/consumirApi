7d9b79e1478ce9785d3052c28c167900bae92850
PalomequeDeLaTorre <carlospd.ti22@utsjr.edu.mx>
2024-02-02 13:26:57 -0600

    consumirApi

--- src/index.js
+++ src/index.js
@@ -2,14 +2,13 @@ import React from "react";
 import ReactDom from "react-dom/client";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
-import { Inicio } from "./components/Inicio";
 import { Productos } from "./components/Productos";
 
 const root=ReactDom.createRoot(document.getElementById("root"));
 
 root.render(
     <>
-        <Inicio />
+
         <Productos />
 
     </>
