1f0b8d079bc0a6c5fe0bd407dc8bd65ccdb6c9dd
PalomequeDeLaTorre <carlospd.ti22@utsjr.edu.mx>
2024-02-20 08:45:51 -0600

    insertarApi

--- src/config/rutas.js
+++ src/config/rutas.js
@@ -1,15 +1,6 @@
+//Usuarios
 export const URL_API="https://apifirebase-0npp.onrender.com/api/";
 export const URL_IMAGES="https://apifirebase-0npp.onrender.com/images";
+//Productos
 
 
-// Usuarios
-export const URL_API3="https://ejemlofirebase.onrender.com/usuarios//api/";
-
-export const URL_API2="https://ejemlofirebase.onrender.com/usuarios/api/";
-
-export const IMG_API="https://ejemlofirebase.onrender.com/uploads/";
-
-
-// Productos 
-
-export const URL_API_PRODUCTOS="https://ejemlofirebase.onrender.com/productos/productos/api/";
